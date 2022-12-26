a0a57c8f137f337cfef1e0335f65e03964218989
Developer <sherzod88aliyev@gmail.com>
2022-12-27 01:50:40 +0500

    add dark && light mode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-var click = document.querySelector('.click');
-var content_block = document.querySelector('.content_block');
+"use strict";
 
-click.addEventListener('click', function(e) {
-    content_block.classList.toggle('hidden');
-    content_block.classList.toggle('active');
-})
+// Selectors:
+const body = document.querySelector("body");
+const sunBtn = document.getElementById("sun");
+const moonBtn = document.getElementById("moon");
+const figure = document.querySelector(".accaunt_info");
+const figureItem = document.querySelector(".accaunt_info .triangle");
+
+// EventListener:
+moonBtn.addEventListener("click", darkMode);
+sunBtn.addEventListener("click", lightMode);
+
+// Functions:
+function darkMode() {
+  body.className = "dark";
+  body.classList.add("dark-color");
+  sunBtn.className =
+    sunBtn.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+  moonBtn.className =
+    moonBtn.className == "bx bx-moon" ? "bx bxs-moon" : "bx bx-moon";
+  figure.classList.add("active");
+}
+
+function lightMode() {
+  body.className = "light";
+  sunBtn.className =
+    sunBtn.className == "bx bx-sun" ? "bx bxs-sun" : "bx bx-sun";
+  moonBtn.className =
+    moonBtn.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  figure.classList.remove("active");
+}
